648d2e240beaa3591ebf12401c473ad2c1c747d5
Selene <149156667+joanapq@users.noreply.github.com>
2024-01-14 13:35:06 +0000

    three quiz questions and answers added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,30 @@ const choiceAnswers = document.getElementById("choices");
 
 let questionIndex = 0;
 let score = 0;
+
+const questions = [
+    {
+        question: "Who did it?",
+        answers: [
+            { text: "Lady Violet", correct: true},
+            { text: "Vice President Mauve", correct: false},
+            { text: "Duchess of Vermillion", correct: false},
+        ]
+    },
+    {
+        question: "Where?",
+        answers: [
+            { text: "The central fire", correct: false},
+            { text: "The ancient ruins", correct: false},
+            { text: "The thick forest", correct: true},
+        ]
+    },
+    {
+        question: "With what?",
+        answers: [
+            { text: "A cauldron", correct: false},
+            { text: "A log", correct: false},
+            { text: "A broom", correct: true},
+        ]
+    }
+];
