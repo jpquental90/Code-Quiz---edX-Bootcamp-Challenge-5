5207b4863310c5c4d2c7c7127fc41ebbf5c56136
Selene <149156667+joanapq@users.noreply.github.com>
2024-01-15 20:34:22 +0000

    added comments to scores.js and changed order of things

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,16 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-    displayHighScores();
+displayHighScores();
 
-    let clearButton = document.getElementById("clear");
-    clearButton.addEventListener("click", clearHighScores);
-});
+
+// Links 'Clear Highscores' button with the relevant HTML element, a 'click' event and the 'clearHighScores' function below.
+
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearHighScores);
+
+
+//Function to clear Highscores from the displayed page and local storage.
+
+function clearHighScores() {
+    localStorage.removeItem("highScores");
+    displayHighScores();
+}
+
+
+// Function indicating how the highscores should be displayed on the page, retrieving the relevant data from local storage and setting the text format for each entry, which are added as list items.
 
 function displayHighScores() {
     let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
     let highScoresList = document.getElementById("highscores");
 
-    // Clear the existing list
     highScoresList.innerHTML = "";
 
     highScores.forEach((highScore, index) => {
@@ -18,12 +29,4 @@ function displayHighScores() {
         listItem.textContent = `${highScore.initials} - Score: ${highScore.score} - Time Left: ${highScore.timeLeft}`;
         highScoresList.appendChild(listItem);
     });
-}
-
-function clearHighScores() {
-    // Clear the high scores from local storage
-    localStorage.removeItem("highScores");
-
-    // Refresh the displayed high scores
-    displayHighScores();
 }
