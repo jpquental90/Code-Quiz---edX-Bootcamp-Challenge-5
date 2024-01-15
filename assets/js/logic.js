a24e1bb53100f4756d8cd10c36d626da1f33deee
Selene <149156667+joanapq@users.noreply.github.com>
2024-01-15 18:53:57 +0000

    created separate js files for questions and logic, for the first index.html file, going back to the suggested structure from the starter code, as it seems to be better for organisation

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -15,32 +15,7 @@ const incorrectSound = new Audio("./assets/sfx/incorrect.wav");
 let questionIndex;
 let score;
 
-const questionsQuiz = [
-    {
-        title: "The TV series 'Foundation' is based on the books by which author?",
-        choices: [
-            { text: "Robert A. Heinlein", correct: false},
-            { text: "Isaac Asimov", correct: true},
-            { text: "Arthur C. Clarke", correct: false},
-        ]
-    },
-    {
-        title: "In which year was the first season released?",
-        choices: [
-            { text: "2021", correct: true},
-            { text: "2022", correct: false},
-            { text: "2023", correct: false},
-        ]
-    },
-    {
-        title: "In the series, what are the names of the three co-existing clone versions of different ages of emperor Cleo?",
-        choices: [
-            { text: "Sunrise, Sunset, Twilight", correct: false},
-            { text: "Green, Amber, Red", correct: false},
-            { text: "Dawn, Day, Dusk", correct: true},
-        ]
-    }
-];
+
 
 startButton.addEventListener("click", function() {
     questionIndex = 0;
---
+++ assets/js/questions.js
@@ -0,0 +1,26 @@
+const questionsQuiz = [
+    {
+        title: "The TV series 'Foundation' is based on the books by which author?",
+        choices: [
+            { text: "Robert A. Heinlein", correct: false},
+            { text: "Isaac Asimov", correct: true},
+            { text: "Arthur C. Clarke", correct: false},
+        ]
+    },
+    {
+        title: "In which year was the first season released?",
+        choices: [
+            { text: "2021", correct: true},
+            { text: "2022", correct: false},
+            { text: "2023", correct: false},
+        ]
+    },
+    {
+        title: "In the series, what are the names of the three co-existing clone versions of different ages of emperor Cleo?",
+        choices: [
+            { text: "Sunrise, Sunset, Twilight", correct: false},
+            { text: "Green, Amber, Red", correct: false},
+            { text: "Dawn, Day, Dusk", correct: true},
+        ]
+    }
+];
